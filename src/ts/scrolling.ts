f2e35ed89eebc42e6938b4fe3caa4497e213c5c6
Michael Neu <git@michaeln.eu>
2017-10-29 20:00:10 +0100

    add smooth scrolling for navbar

---
+++ src/ts/scrolling.ts
@@ -0,0 +1,31 @@
+interface JumpOptions {
+  duration?: number;
+  offset?: number;
+  callback?: () => void;
+  easing?: (t: number, b: number, c: number, d: number) => number;
+  a11y?: boolean;
+}
+
+declare function Jump(selector: string, options?: JumpOptions) : void;
+
+(function () {
+  const links = document.querySelectorAll("nav ul li a");
+
+  for (let i = 0; i < links.length; i++) {
+    const link = links.item(i) as HTMLLinkElement;
+
+    link.onclick = (event: MouseEvent) => {
+      const target = link.href
+                         .replace(/[^#]*#(.*)/, "[name='$1']");
+
+      Jump(target, {
+        duration: 200
+      });
+
+      event.preventDefault();
+      event.stopPropagation();
+
+      return true;
+    };
+  }
+})();
