95567648cf2981864a0a39f2f3de45161c188155
Michael Neu <git@michaeln.eu>
2017-11-02 20:24:28 +0100

    add gmaps venue

---
+++ src/ts/map.ts
@@ -0,0 +1,157 @@
+google.maps.event.addDomListener(window, "load", () => {
+  const position = new google.maps.LatLng(49.003169, 12.096859),
+        container = document.querySelector(".venue #map"),
+        map = new google.maps.Map(container, {
+          zoom: 15,
+          scrollwheel: false,
+          center: position,
+          styles: [
+            {
+              featureType: "all",
+              elementType: "labels.text",
+              stylers: [
+                {
+                  color: "#8bc34a"
+                },
+                {
+                  weight: 0.37
+                }
+              ]
+            },
+            {
+              featureType: "administrative",
+              elementType: "labels.text.fill",
+              stylers: [
+                {
+                  color: "#444444"
+                }
+              ]
+            },
+            {
+              featureType: "landscape",
+              elementType: "all",
+              stylers: [
+                {
+                  color: "#f2f2f2"
+                }
+              ]
+            },
+            {
+              featureType: "poi",
+              elementType: "all",
+              stylers: [
+                {
+                  visibility: "off"
+                }
+              ]
+            },
+            {
+              featureType: "poi.attraction",
+              elementType: "labels.icon",
+              stylers: [
+                {
+                  visibility: "on"
+                },
+                {
+                  saturation: 0
+                },
+                {
+                  weight: 2.00
+                }
+              ]
+            },
+            {
+              featureType: "poi.business",
+              elementType: "labels.icon",
+              stylers: [
+                {
+                  visibility: "on"
+                }
+              ]
+            },
+            {
+              featureType: "poi.government",
+              elementType: "labels.icon",
+              stylers: [
+                {
+                  visibility: "on"
+                }
+              ]
+            },
+            {
+              featureType: "poi.park",
+              elementType: "labels.icon",
+              stylers: [
+                {
+                  visibility: "on"
+                }
+              ]
+            },
+            {
+              featureType: "poi.school",
+              elementType: "labels.icon",
+              stylers: [
+                {
+                  visibility: "on"
+                }
+              ]
+            },
+            {
+              featureType: "road",
+              elementType: "all",
+              stylers: [
+                {
+                  saturation: -100
+                },
+                {
+                  lightness: 45
+                }
+              ]
+            },
+            {
+              featureType: "road.highway",
+              elementType: "all",
+              stylers: [
+                {
+                  visibility: "simplified"
+                }
+              ]
+            },
+            {
+              featureType: "road.arterial",
+              elementType: "labels.icon",
+              stylers: [
+                {
+                  visibility: "off"
+                }
+              ]
+            },
+            {
+              featureType: "transit",
+              elementType: "all",
+              stylers: [
+                {
+                  visibility: "off"
+                }
+              ]
+            },
+            {
+              featureType: "water",
+              elementType: "all",
+              stylers: [
+                {
+                  color: "#8bc34a"
+                },
+                {
+                  visibility: "on"
+                }
+              ]
+            }
+          ]
+        });
+
+  new google.maps.Marker({
+    position,
+    map
+  });
+});
