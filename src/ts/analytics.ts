ef3ff439e497b62702ebdb0c0152a6a0e75fb61a
Michael Neu <git@michaeln.eu>
2017-12-15 00:51:12 +0100

    fix typescript compiler errors

--- src/ts/analytics.ts
+++ src/ts/analytics.ts
@@ -1,10 +1,17 @@
+type DataLayerElement = any;
+
+interface Window {
+  dataLayer: DataLayerElement[];
+}
+
 (function () {
   window.dataLayer = window.dataLayer || [];
 
-  function gtag() {
-    dataLayer.push(arguments);    
+  function gtag(...args: DataLayerElement[]) {
+    window.dataLayer
+          .push(args);
   }
-  gtag('js', new Date());
 
-  gtag('config', 'UA-90366502-1');
-})
+  gtag("js", new Date());
+  gtag("config", "UA-90366502-1");
+})();
