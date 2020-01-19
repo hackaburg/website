cd46984d5cd206d7466fd689a9a9e250ac6fc19e
Michael Neu <git@michaeln.eu>
2020-01-19 19:18:40 +0100

    add index navbar items

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,44 @@
 import * as React from "react";
+import { Anchors } from "../anchors";
+import { Landing } from "../components/landing";
 import { Navbar } from "../components/navbar";
 import { Page } from "../components/page";
 
 export default () => (
   <Page>
-    <Navbar items={[]} />
+    <Navbar
+      items={[
+        {
+          label: "About",
+          marker: Anchors.About,
+        },
+        {
+          label: "Tracks",
+          marker: Anchors.Tracks,
+        },
+        {
+          label: "Sponsors",
+          marker: Anchors.Sponsors,
+        },
+        {
+          label: "Hackaburg 2019",
+          marker: Anchors.Hackaburg2019,
+        },
+        {
+          label: "Schedule",
+          marker: Anchors.Schedule,
+        },
+        {
+          label: "Teaser",
+          marker: Anchors.Teaser,
+        },
+        {
+          label: "Venue",
+          marker: Anchors.Venue,
+        },
+      ]}
+    />
+
+    <Landing />
   </Page>
 );
