cb85c50397901e195a936f3900f9b7f977404d4a
Michael Neu <git@michaeln.eu>
2020-01-19 23:26:03 +0100

    add tracks

---
+++ src/components/tracks.tsx
@@ -0,0 +1,95 @@
+import styled from "@emotion/styled";
+import * as React from "react";
+import { Container } from "./container";
+import { Column } from "./grid/column";
+import { Row } from "./grid/row";
+import { Header } from "./header";
+import { Track } from "./track";
+
+const TracksContainer = styled.div`
+  margin: 5rem 0;
+`;
+
+const TrackContainer = styled.div`
+  padding: 1rem 2rem 1rem 0;
+`;
+
+export const Tracks = () => (
+  <Container>
+    <TracksContainer>
+      <Header title="Tracks" subtitle="How you can change the future" />
+
+      <Row>
+        <Column>
+          <TrackContainer>
+            <Track
+              description="How can we connect people with the same destination: the local football arena? Find digital and sustainable solutions to connect those people to foster shared mobility."
+              imageURL="images/tracks/connected-mobility.jpg"
+              sponsor="SSV Jahn Regensburg"
+              title="A common goal"
+            />
+          </TrackContainer>
+        </Column>
+
+        <Column>
+          <TrackContainer>
+            <Track
+              description="How can we connect people with the same destination: the local football arena? Find digital and sustainable solutions to connect those people to foster shared mobility."
+              imageURL="images/tracks/connected-mobility.jpg"
+              sponsor="SSV Jahn Regensburg"
+              title="A common goal"
+            />
+          </TrackContainer>
+        </Column>
+      </Row>
+
+      <Row>
+        <Column>
+          <TrackContainer>
+            <Track
+              description="How can we connect people with the same destination: the local football arena? Find digital and sustainable solutions to connect those people to foster shared mobility."
+              imageURL="images/tracks/connected-mobility.jpg"
+              sponsor="SSV Jahn Regensburg"
+              title="A common goal"
+            />
+          </TrackContainer>
+        </Column>
+
+        <Column>
+          <TrackContainer>
+            <Track
+              description="How can we connect people with the same destination: the local football arena? Find digital and sustainable solutions to connect those people to foster shared mobility."
+              imageURL="images/tracks/connected-mobility.jpg"
+              sponsor="SSV Jahn Regensburg"
+              title="A common goal"
+            />
+          </TrackContainer>
+        </Column>
+      </Row>
+
+      <Row>
+        <Column>
+          <TrackContainer>
+            <Track
+              description="How can we connect people with the same destination: the local football arena? Find digital and sustainable solutions to connect those people to foster shared mobility."
+              imageURL="images/tracks/connected-mobility.jpg"
+              sponsor="SSV Jahn Regensburg"
+              title="A common goal"
+            />
+          </TrackContainer>
+        </Column>
+
+        <Column>
+          <TrackContainer>
+            <Track
+              description="How can we connect people with the same destination: the local football arena? Find digital and sustainable solutions to connect those people to foster shared mobility."
+              imageURL="images/tracks/connected-mobility.jpg"
+              sponsor="SSV Jahn Regensburg"
+              title="A common goal"
+            />
+          </TrackContainer>
+        </Column>
+      </Row>
+    </TracksContainer>
+  </Container>
+);
