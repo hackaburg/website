3d5fda44f7a4c7d008b791e356416c9715deed64
Michael Neu <git@michaeln.eu>
2020-01-20 12:44:04 +0100

    add schedule anchor

--- src/components/schedule/index.tsx
+++ src/components/schedule/index.tsx
@@ -1,4 +1,6 @@
 import * as React from "react";
+import { Anchors } from "../../anchors";
+import { useScrollSpyMarker } from "../../hooks/use-scrollspy";
 import { Container } from "../container";
 import { Column } from "../grid/column";
 import { Row } from "../grid/row";
@@ -8,60 +10,65 @@ import { Day } from "./days";
 import { Event, Spacer } from "./event";
 import { Timetable } from "./timetable";
 
-export const Schedule = () => (
-  <Container>
-    <Header title="Schedule" subtitle="When and what" />
-    <Timetable>
-      <Day date="Thu, 14.05.2020">
-        <Event hours={2} minutes={0} time="08:00 - 09:30" title="Breakfast" />
-        <Event hours={2} minutes={0} time="08:00 - 09:30" title="Breakfast" />
-        <Spacer hours={2} minutes={0} />
-        <Event
-          hours={2}
-          minutes={0}
-          time="08:00 - 09:30"
-          title="Breakfast"
-          accent
-        />
-        <Event
-          hours={2}
-          minutes={0}
-          time="08:00 - 09:30"
-          title="Breakfast"
-          striped
-        />
-        <Event hours={2} minutes={0} time="08:00 - 09:30" title="Breakfast" />
-      </Day>
-      <Day date="Fri, 15.05.2020">
-        <Event hours={4} minutes={0} time="08:00 - 09:30" title="Breakfast" />
-      </Day>
-      <Day date="Sat, 16.05.2020">
-        <Event hours={6} minutes={0} time="08:00 - 09:30" title="Breakfast">
-          <Row>
-            <Column>
-              <Event
-                hours={1}
-                minutes={0}
-                time="08:00 - 09:30"
-                title="Breakfast"
-              />
-            </Column>
+export const Schedule = () => {
+  const marker = useScrollSpyMarker(Anchors.Schedule);
+  return (
+    <Container>
+      {marker}
 
-            <Column>
-              <Event
-                hours={1}
-                minutes={0}
-                time="08:00 - 09:30"
-                title={
-                  <Hint superscript="1" text="Test">
-                    Breakfast
-                  </Hint>
-                }
-              />
-            </Column>
-          </Row>
-        </Event>
-      </Day>
-    </Timetable>
-  </Container>
-);
+      <Header title="Schedule" subtitle="When and what" />
+      <Timetable>
+        <Day date="Thu, 14.05.2020">
+          <Event hours={2} minutes={0} time="08:00 - 09:30" title="Breakfast" />
+          <Event hours={2} minutes={0} time="08:00 - 09:30" title="Breakfast" />
+          <Spacer hours={2} minutes={0} />
+          <Event
+            hours={2}
+            minutes={0}
+            time="08:00 - 09:30"
+            title="Breakfast"
+            accent
+          />
+          <Event
+            hours={2}
+            minutes={0}
+            time="08:00 - 09:30"
+            title="Breakfast"
+            striped
+          />
+          <Event hours={2} minutes={0} time="08:00 - 09:30" title="Breakfast" />
+        </Day>
+        <Day date="Fri, 15.05.2020">
+          <Event hours={4} minutes={0} time="08:00 - 09:30" title="Breakfast" />
+        </Day>
+        <Day date="Sat, 16.05.2020">
+          <Event hours={6} minutes={0} time="08:00 - 09:30" title="Breakfast">
+            <Row>
+              <Column>
+                <Event
+                  hours={1}
+                  minutes={0}
+                  time="08:00 - 09:30"
+                  title="Breakfast"
+                />
+              </Column>
+
+              <Column>
+                <Event
+                  hours={1}
+                  minutes={0}
+                  time="08:00 - 09:30"
+                  title={
+                    <Hint superscript="1" text="Test">
+                      Breakfast
+                    </Hint>
+                  }
+                />
+              </Column>
+            </Row>
+          </Event>
+        </Day>
+      </Timetable>
+    </Container>
+  );
+};
