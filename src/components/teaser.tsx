f4e4c64de2057d77e3988108bacce411f8b83636
Bastian Meissner <meissner.bastian@gmail.com>
2020-01-25 12:57:28 +0100

    Prettier

--- src/components/teaser.tsx
+++ src/components/teaser.tsx
@@ -83,13 +83,21 @@ export const Teaser = () => {
             <Text>Hackaburg 2020 has finally started!</Text>
             <Text>Stay up to date on our social media channels</Text>
 
-            <Link label="Hackaburg Instagram" target="_blank" to="https://www.instagram.com/hackaburg/">
+            <Link
+              label="Hackaburg Instagram"
+              target="_blank"
+              to="https://www.instagram.com/hackaburg/"
+            >
               <Button fluid>Follow us on Instagram</Button>
             </Link>
 
             <br />
 
-            <Link label="Hackaburg Facebook" target="_blank" to="https://www.facebook.com/hackaburg/">
+            <Link
+              label="Hackaburg Facebook"
+              target="_blank"
+              to="https://www.facebook.com/hackaburg/"
+            >
               <Button fluid>Follow us on Facebook</Button>
             </Link>
           </Column>
