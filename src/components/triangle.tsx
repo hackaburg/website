f6da12caf1c8a414f3373b6e6ae00ce78622029a
Michael Neu <git@michaeln.eu>
2020-01-29 13:06:27 +0100

    fix horizontal scrolling in aftermovie

--- src/components/triangle.tsx
+++ src/components/triangle.tsx
@@ -3,21 +3,18 @@ import styled from "@emotion/styled";
 import * as React from "react";
 import { triangleSize } from "../constants";
 
-const Base = styled.div`
+const TriangleContainer = styled.div`
   position: relative;
   width: 100%;
+  overflow-x: hidden;
+  height: ${triangleSize};
+  top: -${triangleSize};
+`;
 
-  &:before {
-    content: " ";
-    display: block;
-
-    position: absolute;
-    bottom: 0;
-    width: 0;
-    height: 0;
-
-    border-style: solid;
-  }
+const Base = styled.div`
+  width: 0;
+  height: 0;
+  border-style: solid;
 `;
 
 interface ITriangleProps {
@@ -25,45 +22,45 @@ interface ITriangleProps {
 }
 
 export const UpwardsLeftTriangle = ({ color = "white" }: ITriangleProps) => (
-  <Base
-    css={css`
-      &:before {
+  <TriangleContainer>
+    <Base
+      css={css`
         border-width: ${triangleSize} 0rem 0rem 100vw;
         border-color: transparent transparent transparent ${color};
-      }
-    `}
-  />
+      `}
+    />
+  </TriangleContainer>
 );
 
 export const UpwardsRightTriangle = ({ color = "white" }: ITriangleProps) => (
-  <Base
-    css={css`
-      &:before {
+  <TriangleContainer>
+    <Base
+      css={css`
         border-width: 0rem 0rem ${triangleSize} 100vw;
         border-color: transparent transparent ${color} transparent;
-      }
-    `}
-  />
+      `}
+    />
+  </TriangleContainer>
 );
 
 export const DownwardsLeftTriangle = ({ color = "white" }: ITriangleProps) => (
-  <Base
-    css={css`
-      &:before {
+  <TriangleContainer>
+    <Base
+      css={css`
         border-width: ${triangleSize} 100vw 0rem 0rem;
         border-color: ${color} transparent transparent transparent;
-      }
-    `}
-  />
+      `}
+    />
+  </TriangleContainer>
 );
 
 export const DownwardsRightTriangle = ({ color = "white" }: ITriangleProps) => (
-  <Base
-    css={css`
-      &:before {
+  <TriangleContainer>
+    <Base
+      css={css`
         border-width: 0rem 100vw ${triangleSize} 0rem;
         border-color: transparent ${color} transparent transparent;
-      }
-    `}
-  />
+      `}
+    />
+  </TriangleContainer>
 );
