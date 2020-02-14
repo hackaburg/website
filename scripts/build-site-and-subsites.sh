7a76e13a1d0059ce4dc0f3c2c0d358014974f52c
Michael Neu <git@michaeln.eu>
2020-02-14 20:05:08 +0100

    also build dev branches

--- scripts/build-site-and-subsites.sh
+++ scripts/build-site-and-subsites.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-# build the current site
+
+# patterns to identify whether year tags and dev branches
+year_tag_pattern="hb-*"
+dev_branch_pattern="dev/*"
+
+# we'll restore the original branch at the end
+original_branch=$(git rev-parse --abbrev-ref HEAD)
+
+git checkout master
+yarn install
 yarn build
 
 # backup the current site
@@ -8,24 +17,38 @@ tmp_folder_name=$(mktemp -u XXXXX)
 mv $output_folder_name $tmp_folder_name
 
 # get all tags prefixed with "hb-", as those are the previous years
-all_year_tags=$(git tag -l hb-*)
+all_year_tags=$(git tag -l $year_tag_pattern)
 
-for tag in $all_year_tags; do
-  year=${tag:3}
-  echo "building site for $year"
+# get all dev branches
+all_dev_branches=$(git branch --list $dev_branch_pattern | cut -c 3-)
 
-  git checkout $tag
+# combine build targets
+printf -v all_builds $"$all_year_tags\n$all_dev_branches"
+
+for build in $all_builds; do
+  if [[ $build == hb-* ]]; then
+    build_name=${build:3}
+  else
+    build_name=$build
+
+    dev_folder_name=$(dirname "$build")
+    mkdir -p "$tmp_folder_name/$dev_folder_name"
+  fi
+
+  echo "building site for $build"
+
+  git checkout $build
   if [[ $? != 0 ]]; then
     exit $?
   fi
 
   yarn install
   yarn build
-  mv $output_folder_name "$tmp_folder_name/$year"
+  mv $output_folder_name "$tmp_folder_name/$build_name"
 done
 
 mv $tmp_folder_name $output_folder_name
 
-# restore master branch
-git checkout master
+# restore original_branch branch
+git checkout $original_branch
 yarn install
