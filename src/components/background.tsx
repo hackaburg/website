4ee26102d59941c2fc6f6c3a2def0e40cc982a4d
Max Pöschl <poeschl.max@gmail.com>
2020-02-27 23:46:10 +0100

    Change stars.svg background image and introduce background-size for mobile version

--- src/components/background.tsx
+++ src/components/background.tsx
@@ -1,6 +1,7 @@
 import styled from "@emotion/styled";
 import * as React from "react";
 import { usePublicFileURL } from "../hooks/use-public-file-url";
+import {breakpointMobile} from "../constants";
 
 interface IBackgroundDivProps {
   imageUrl: string | undefined;
@@ -10,6 +11,10 @@ const BackgroundDiv = styled.div<IBackgroundDivProps>`
   background-image: url(${(props) => props.imageUrl});
   background-color: #040b1a;
   color: white;
+
+  @media screen and (max-width: ${breakpointMobile}) {
+    background-size: 535px 535px;
+  }
 `;
 
 interface IBackgroundProps {
