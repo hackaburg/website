94058ab3ac5b299c7f948a0f4244268581a5fdc7
Bastian Meissner <phntxx@users.noreply.github.com>
2022-05-12 13:11:19 +0200

    Add new track

--- src/components/tracks.tsx
+++ src/components/tracks.tsx
@@ -29,21 +29,20 @@ export const Tracks = () => {
           <Column grow={1}>
             <TrackContainer>
               <Track
-                description="Your device is checking at the entrance gate to the TechBase underground parking and identifies the type of engine between gasoline, diesel and electric. In addition, two- wheelers should be identified and differentiated according to their engine (human, gasoline or electric) too."
-                imageURL="images/tracks/sound-of-propulsion.png"
-                sponsor="Infineon"
-                title="The Sound of Propulsion"
-              />
-            </TrackContainer>
-          </Column>
-
-          <Column grow={1}>
-            <TrackContainer>
-              <Track
                 description="How can digital tools - with regards to situations like the COVID-19 pandemic - elevate collaboration and networking within a company to a whole new level?"
                 imageURL="images/tracks/new-ways-to-work.jpg"
                 sponsor="Bayernwerk"
                 title="New ways to work"
+              />
+            </TrackContainer>
+          </Column>
+          <Column grow={1}>
+            <TrackContainer>
+              <Track
+                description="How can we effectively use our plant resources in times with pandemic and climate change?"
+                imageURL="images/tracks/smart-sustainability-plant.png"
+                sponsor="Continental"
+                title="Smart Sustainability Plant"
               />
             </TrackContainer>
           </Column>
@@ -52,10 +51,20 @@ export const Tracks = () => {
           <Column grow={1}>
             <TrackContainer>
               <Track
-                description="How can we effectively use our plant resources in times with pandemic and climate change?"
-                imageURL="images/tracks/smart-sustainability-plant.png"
-                sponsor="Continental"
-                title="Smart Sustainability Plant"
+                description="Your device is checking at the entrance gate to the TechBase underground parking and identifies the type of engine between gasoline, diesel and electric. In addition, two- wheelers should be identified and differentiated according to their engine (human, gasoline or electric) too."
+                imageURL="images/tracks/sound-of-propulsion.png"
+                sponsor="Infineon"
+                title="The Sound of Propulsion"
+              />
+            </TrackContainer>
+          </Column>
+          <Column grow={1}>
+            <TrackContainer>
+              <Track
+                description="How can we minimise the impact of increased living comfort on climate change by intelligently regulating all components?"
+                imageURL="images/tracks/climate-intelligent-house.jpg"
+                sponsor="Wolf"
+                title="The climate intelligent house"
               />
             </TrackContainer>
           </Column>
