b228b47838069e6830ff8e8f9f5498127ce1e06c
Markus Guder <markus.guder@gmail.com>
2023-02-01 08:09:36 +0100

    calendar is partially working

--- src/routes/landing/Schedule.svelte
+++ src/routes/landing/Schedule.svelte
@@ -1,11 +1,21 @@
 <script>
 	import BaseComponent from '../components/BaseComponent.svelte';
 	import Day from '../components/Day.svelte';
+	import Event from '../components/Event.svelte';
+	import { StartType } from '../models/enum';
 </script>
 
 <BaseComponent headline={'Schedule'} subHeadline={'When and what?'} id={'green'}>
 	<div class="grid grid-cols-3">
-		<Day headline={'Do. 25.6.2023'} />
+		<Day headline={'Do. 25.6.2023'}>
+			<Event
+				headline="Breakfast"
+				duration={60}
+				start={[17, 0]}
+				startType={StartType.AM}
+				color="hackaburg"
+			/>
+		</Day>
 		<Day headline={'Fr. 25.6.2023'} />
 		<Day headline={'Sa. 25.6.2023'} />
 	</div>
