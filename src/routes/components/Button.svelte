196155a8b501209310f8ca38820f10103ba53dd6
LMN <43175590+nekoumyu@users.noreply.github.com>
2025-05-14 17:26:26 +0200

    adds discord button

--- src/routes/components/Button.svelte
+++ src/routes/components/Button.svelte
@@ -1,11 +1,15 @@
 <script lang="ts">
 	export let label: string;
+	export let type = "default";
+	import discordIcon from '$lib/images/discord.svg';
 </script>
 
 <div class="relative group">
 	<button
-		class="glow relative bg-tungsten m-auto w-full mt-8 border-2 border-white rounded-3xl px-3 py-2 dark:text-black hover:cursor-pointer hover:bg-deadlydepth"
-	>
+		class="flex items-center gap-1 justify-center glow relative bg-tungsten m-auto w-full mt-8 border-2 border-white rounded-3xl px-3 py-2 dark:text-black hover:cursor-pointer hover:bg-deadlydepth">
 		{label}
+		{#if type === "discord"}
+		<img class="h-5" src={discordIcon} alt="" />
+		{/if}
 	</button>
 </div>
